8d24779f8cf57a3a3cdfb5251974799f3d4586f5
KaleTinvald <100299022+KaleTinvald@users.noreply.github.com>
2025-04-27 01:54:55 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Сторінка повністю завантажена!");
+  
+    const form = document.querySelector('form');
+    if(form) {
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('Дякуємо за ваше повідомлення!');
+        form.reset();
+      });
+    }
+  });
+  
