2f76ac1db3cd65541066453ebe0454fe7fe69ef7
KaleTinvald <100299022+KaleTinvald@users.noreply.github.com>
2025-04-29 23:34:55 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Сторінка повністю завантажена!");
-    const img = document.querySelector('.zoom-image');
-    img.addEventListener('click', () => {
-      img.classList.toggle('zoomed');
-    });
     const form = document.querySelector('form');
     if(form) {
       form.addEventListener('submit', (e) => {
