cad1e5a8d26c11b12314281a6f748aed8b29245e
KaleTinvald <100299022+KaleTinvald@users.noreply.github.com>
2025-05-06 08:47:17 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,24 +9,3 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
-letlet lastScrollY = window.scrollY;
-
-document.addEventListener("DOMContentLoaded", function () {
-  const img = document.querySelector("header img");
-
-  window.addEventListener("scroll", function () {
-    const scrollY = window.scrollY;
-    const fadeStart = 0;
-    const fadeEnd = 300;
-
-    let opacity = 1;
-
-    if (scrollY >= fadeStart && scrollY <= fadeEnd) {
-      opacity = 1 - (scrollY - fadeStart) / (fadeEnd - fadeStart);
-    } else if (scrollY > fadeEnd) {
-      opacity = 0;
-    }
-
-    img.style.opacity = opacity;
-  });
-});
